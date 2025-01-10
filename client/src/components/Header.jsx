065af5c2ca9892625254a21e835d8ffb2c0bb9bf
sastika <sastikaregmi746@gmail.com>
2025-01-10 23:33:02 +0545

    header update ,photo assets

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,26 +4,41 @@ import logo from '../assets/logo.png'
 const Header = () => {
   return (
     <header className='h-20 shadow-md sticky top-0'>
-        <div className='container mx-auto items-center h-full'>
+        <div className='container mx-auto flex items-center h-full px-2 justify-between'>
 
        {/**logo */}
          <div className='h-full'>
-            <div className='h-full flex items-center justify-center items-center'>
+            <div className='h-full flex justify-center items-center'>
                 <img
                 src={logo}
                 width={170}
                 height={60}
                 alt='logo'
+                className='hidden lg:block'
                 />
                   
+                  <img
+                src={logo}
+                width={120}
+                height={60}
+                alt='logo'
+                className='lg:hidden'
+                />
                 
             </div>
         
 
        {/**Search */}
+       <div>
+          search  
+       </div>
 
 
        {/**login and my cart  */}
+       <div>
+        Login and my cart
+       </div>
+       
        </div>
        </div>
         </header>
