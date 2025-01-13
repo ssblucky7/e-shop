52ee7a84b42f73d095fe54cd5a99966d26b84750
ssblucky7 <SURESHKUMARMAHATOSCI2021@Prime.edu.np>
2025-01-13 21:35:15 +0545

    register and api done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import{ Outlet} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import toast, { Toaster } from'react-hot-toast';
+
 function App() {
   
 
@@ -12,6 +14,7 @@ function App() {
     <Outlet/>
   </main>
   <Footer/>
+  <Toaster/>
 </>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,158 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa6";
+import toast from 'react-hot-toast';
+import Axios from '../utils/Axios';
+import SummaryApi from '../common/SummaryApi';
+import AxiosToastError from '../utils/AxiosToastError';
+import { useNavigate, Link } from'react-router-dom';
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
+    const [data, setData] = React.useState({
+        name : "",
+        email : "",
+        password : "",
+        confirmPassword : ""
+    })
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    }
+
+    const validedValue = Object.values(data).every(el => el)
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        if (data.password !== data.confirmPassword) {
+            toast.error("Password must be the same");
+            return;
+        }
+
+     try {
+        const response =  await Axios({
+            ...SummaryApi.register,
+            data : data
+          })
+
+          if(response.data.error){
+            toast.error(response.data.message);
+          }
+
+          if(response.data.success){
+            toast.success(response.data.message)
+            setData({
+                name : "",
+                email : "",
+                password : "",
+                confirmPassword : ""
+            })
+            navigate("/login")
+          }
+          
+        
+     } catch (error) {
+        AxiosToastError(error)
+        
+     }
+
+       
+
+        
+    }
+
+    return (
+        <section className='w-full container mx-auto px-2'>
+            <div className='bg-white my-4 w-full max-w-lg mx-auto rounded p-7'>
+            <p className="text-center">Welcome To Durable Dress</p>
+
+
+                <form className='grid gap-4 mt-6' onSubmit={handleSubmit}>
+                    <div className='grid gap-1'>
+                        <label htmlFor='name'>Name :</label>
+                        <input 
+                            type='text'
+                            id='name'
+                            autoFocus 
+                            className='bg-blue-50 p-2 border rounded outline-none focus:border-primary-200'
+                            name='name'
+                            value={data.name}
+                            onChange={handleChange}
+                            placeholder='Enter your name'
+                        />
+                    </div>
+
+                    <div className='grid gap-1'>
+                        <label htmlFor='email'>Email :</label>
+                        <input 
+                            type='email'
+                            id='email'
+                            className='bg-blue-50 p-2 border rounded outline-none focus:border-primary-200'
+                            name='email'
+                            value={data.email}
+                            onChange={handleChange}
+                            placeholder='Enter your email'
+                        />
+                    </div>
+
+                    <div className='grid gap-1'>
+                        <label htmlFor='password'>Password :</label>
+                        <div className='bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200'>
+                            <input 
+                                type={showPassword ? "text" : "password"}
+                                id='password'
+                                className='w-full outline-none bg-transparent'
+                                name='password'
+                                value={data.password}
+                                onChange={handleChange}
+                                placeholder='Enter your password'
+                            />
+                            <div onClick={() => setShowPassword(prev => !prev)} className='cursor-pointer'>
+                                {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='grid gap-1'>
+                        <label htmlFor='confirmPassword'>Confirm Password :</label>
+                        <div className='bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200'>
+                            <input 
+                                type={showConfirmPassword ? "text" : "password"}
+                                id='confirmPassword'
+                                className='w-full outline-none bg-transparent'
+                                name='confirmPassword'
+                                value={data.confirmPassword}
+                                onChange={handleChange}
+                                placeholder='Enter your confirm password'
+                            />
+                            <div onClick={() => setShowConfirmPassword(prev => !prev)} className='cursor-pointer'>
+                                {showConfirmPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+                            </div>
+                        </div>
+                    </div>
+
+                    <button 
+                        disabled={!validedValue} 
+                        className={`${validedValue ? "bg-green-800 hover:bg-green-700" : "bg-gray-500"} text-white py-2 rounded font-semibold my-3 tracking-wide`}
+                    >
+                        Register
+                    </button>
+                </form>
+
+                <p>
+                    Already have an account? <Link to={"/login"}
+                    className='font-semibold text-green-7 hover:text-green-800'>Login</Link>
+                </p>
+            </div>
+        </section>
+    )
 }
 
-export default Register
+export default Register;
