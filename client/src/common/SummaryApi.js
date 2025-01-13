ab1998372032a3ad2116d6aed9d6f95ac4d5b628
ssblucky7 <SURESHKUMARMAHATOSCI2021@Prime.edu.np>
2025-01-13 23:52:09 +0545

    update

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -2,7 +2,7 @@ export const baseURL = 'http://localhost:8080'
 
 const SummaryApi = {
     register : {
-        url : '/api/user/register',
+        url : "/api/user/register",
         method : 'post'
     },
     login :{
