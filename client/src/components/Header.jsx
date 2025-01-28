a263a45a736dbd8bdef3866433dc380413f4d9d9
ssblucky7 <SURESHKUMARMAHATOSCI2021@Prime.edu.np>
2025-01-28 23:49:01 +0545

    issues fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,114 +1,102 @@
-import React, { userState } from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.png';
 import Search from './Search';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate, } from 'react-router-dom';
 import { FaRegCircleUser } from "react-icons/fa6";
 import useMobile from '../hooks/useMobile';
 import { BsCart4 } from "react-icons/bs";
 import { useSelector } from 'react-redux';
 import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
+import UserMenu from './UserMenu';
 
 const Header = () => {
-  const [ isMobile ] = useMobile()
-  const location = useLocation()
-  const isSearchPage = location.pathname === "/search"
-  const navigate = useNavigate()
-  const user = useSelector((state)=> state?.user)
-  const [openUserMenu,setOpenUserMenu] = userState(false)
+  const [isMobile] = useMobile();
+  const location = useLocation();
+  const isSearchPage = location.pathname === "/search";
+  const navigate = useNavigate();
+  const user = useSelector((state) => state?.user);
+  const [openUserMenu, setOpenUserMenu] = useState(false); // Fixed useState typo
 
-  console.log('user from store', user)
+  console.log('user from store', user);
 
   const redirectToLoginPage = () => {
-    navigate("/login")
-  }
+    navigate("/login");
+  };
 
   return (
     <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 flex flex-col justify-center gap-1 bg-white'>
-        {
-          !( isSearchPage && isMobile ) && (
-            <div className='container mx-auto flex items-center px-2 justify-between'>
+      {!(isSearchPage && isMobile) && (
+        <div className='container mx-auto flex items-center px-2 justify-between'>
 
-            {/**logo */}
-              <div className='h-full'>
-                 <Link to={"/"} className='h-full flex justify-center items-center'>
-                     <img
-                     src={logo}
-                     width={170}
-                     height={60}
-                     alt='logo'
-                     className='hidden lg:block'
-                     />
-                       
-                       <img
-                     src={logo}
-                     width={120}
-                     height={60}
-                     alt='logo'
-                     className='lg:hidden'
-                     />
-                     
-                 </Link>
-                 </div>
-     
-            {/**Search */}
-            <div className='hidden lg:block'>
-               <Search/>
-            </div>
-     
-     
-            {/**login and my cart  */}
-            <div className=''>
-              {/* user icons display in only mobile mode. */}
+          {/** Logo */}
+          <div className='h-full'>
+            <Link to={"/"} className='h-full flex justify-center items-center'>
+              <img
+                src={logo}
+                width={170}
+                height={60}
+                alt='logo'
+                className='hidden lg:block'
+              />
+              <img
+                src={logo}
+                width={120}
+                height={60}
+                alt='logo'
+                className='lg:hidden'
+              />
+            </Link>
+          </div>
 
-             <button className='text-neutral-600 lg:hidden mt-2'>
-             <FaRegCircleUser size={26}/>
-             </button>
+          {/** Search */}
+          <div className='hidden lg:block'>
+            <Search />
+          </div>
 
-             {/**login and my cart in desktop mode */}
+          {/** Login and My Cart */}
+          <div className=''>
+            {/* User icon (only in mobile mode) */}
+            <button className='text-neutral-600 lg:hidden mt-2'>
+              <FaRegCircleUser size={26} />
+            </button>
+
+            {/** Login and My Cart (Desktop mode) */}
             <div className='hidden lg:flex items-center gap-10'>
-            
-            {
-               user?._id ? (
-                <div className= 'relative'>
-                  <div className='flex items-cener gap-2'>
+
+              {user?._id ? (
+                <div className='relative'>
+                  <div className='flex items-center gap-2'>
                     <p>Account</p>
-                    <GoTriangleDown/>
+                    <GoTriangleDown />
                     {/*< GoTriangleUp/>*/}
                   </div>
-                  <div className = 'absolute right-0 top-12'>
-                      <div className= 'bg-purple-400 rounded p-4 min-w-52 lg: shadow-lg'>
-                        <UserMenu/>
-
-                      </div>
-                      
+                  <div className='absolute right-0 top-12'>
+                    <div className='bg-purple-400 rounded p-4 min-w-52 lg:shadow-lg'>
+                      <UserMenu />
                     </div>
+                  </div>
                 </div>
-               ) : (
+              ) : (
                 <button onClick={redirectToLoginPage} className='text-lg px-2'>Login</button>
-               )
-            }
-              
+              )}
+
               <button className='flex items-center gap-2 bg-green-800 hover:bg-green-700 px-3 py-3 rounded text-white'>
-                {/* add to cart icon */}
+                {/* Add to Cart Icon */}
                 <div className='animate-bounce'>
                   <BsCart4 size={26} />
                 </div>
                 <div className='font-semibold'>
-                 <p>My Cart</p>
+                  <p>My Cart</p>
                 </div>
               </button>
             </div>
-            </div>
-            
-           
-            </div>
+          </div>
+        </div>
+      )}
 
-          )
-        }
-     
-       <div className='container mx-auto px-2 lg:hidden'>
-        <Search/>
-       </div>
+      <div className='container mx-auto px-2 lg:hidden'>
+        <Search />
+      </div>
     </header>
   );
 }
